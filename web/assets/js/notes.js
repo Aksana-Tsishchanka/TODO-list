e101c54191a6035a821333a899c20e8c0b24ffd5
Aksana_Tsishchanka <oks1-92@mail.ru>
2015-04-16 22:49:09 +0300

    Add bower, install angular

--- web/assets/js/notes.js
+++ web/assets/js/notes.js
@@ -1,2 +1,12 @@
-// TODO implement the NotesApp module
-// var NotesApp = ...
+var notesApp = angular.module('NotesApp',[]);
+/*
+	notesApp.run(function($http) {
+		$http.get("/note/get")
+			.success(function(data, status, headers, config) {
+			$scope.notes = data;
+		})
+			.error(function(data, status, headers, config) {
+			console.log(data);
+		});
+	}		
+*/
