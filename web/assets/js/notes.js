0075dd3cc043502a61e165d77c4fc1b1101b5ccf
Aksana-Tsishchanka <oks1-92@mail.ru>
2015-04-17 23:07:24 +0300

    Fetch notes

--- web/assets/js/notes.js
+++ web/assets/js/notes.js
@@ -1,12 +1,40 @@
 var notesApp = angular.module('NotesApp',[]);
-/*
-	notesApp.run(function($http) {
-		$http.get("/note/get")
-			.success(function(data, status, headers, config) {
-			$scope.notes = data;
-		})
-			.error(function(data, status, headers, config) {
-			console.log(data);
-		});
-	}		
-*/
+
+    notesApp.controller('GetNoteCtrl', ['$scope','$http', function($scope, $http) {
+        var note = [];
+        $http.get('note/get').then(
+            function(response){
+                note = response.data;
+
+                $scope.color = note.color;
+                $scope.title = note.title;
+
+                if(note.items) {
+                    $scope.items = note.items;
+                }
+                else {
+                    $scope.items = note.items;
+                }
+            }, 
+            function(errResponse) {
+            console.error('Error while fetching notes');
+            });
+
+            $scope.changeState = function(id, isDone) {
+                console.log(id);
+                $http({
+                    url: 'note/'+ id +'/updateItemIsDone',
+                    method: "POST",
+                    data: { id: id, isDone: true}
+                })
+                .then(function(response) {
+                    // success
+                    console.log(response);
+                }, 
+                function(response) { // optional
+                // failed
+                    }
+                );
+            }
+    }]); 
+
